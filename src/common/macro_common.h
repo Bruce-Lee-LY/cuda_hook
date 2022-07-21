bc7f694d0e7619ea52f1042583c8fb5b1197d7db
Bruce-Lee-LY <yong-li14@tsinghua.org.cn>
2022-07-21 23:01:13 +0800

    add nvblas

--- src/common/macro_common.h
+++ src/common/macro_common.h
@@ -18,16 +18,39 @@
 #define HOOK_LIKELY(x) __builtin_expect(!!(x), 1)
 #define HOOK_UNLIKELY(x) __builtin_expect(!!(x), 0)
 
+inline char *curr_time() {
+    time_t raw_time = time(nullptr);
+    struct tm *time_info = localtime(&raw_time);
+    static char now_time[64];
+    now_time[strftime(now_time, sizeof(now_time), "%Y-%m-%d %H:%M:%S", time_info)] = '\0';
+
+    return now_time;
+}
+
+inline int get_pid() {
+    static int pid = getpid();
+
+    return pid;
+}
+
+inline long int get_tid() {
+    thread_local long int tid = syscall(SYS_gettid);
+
+    return tid;
+}
+
 #define HOOK_LOG_TAG "CUDA-HOOK"
 #define HOOK_LOG_FILE(x) (strrchr(x, '/') ? (strrchr(x, '/') + 1) : x)
-#define HLOG(format, ...)                                                                             \
-    fprintf(stderr, "[%s %d:%ld %s:%d %s] " format "\n", HOOK_LOG_TAG, getpid(), syscall(SYS_gettid), \
-            HOOK_LOG_FILE(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define HLOG(format, ...)                                                                                        \
+    do {                                                                                                         \
+        fprintf(stderr, "[%s %s %d:%ld %s:%d %s] " format "\n", HOOK_LOG_TAG, curr_time(), get_pid(), get_tid(), \
+                HOOK_LOG_FILE(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__);                                 \
+    } while (0)
 
 #define HOOK_CHECK(x)                     \
     do {                                  \
         if (HOOK_UNLIKELY(!(x))) {        \
-            HLOG("check failed: %s", #x); \
+            HLOG("Check failed: %s", #x); \
             exit(EXIT_FAILURE);           \
         }                                 \
     } while (0)
